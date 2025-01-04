1e5fa8aeef4b9af44af2aab0e21fc80143d805e7
KazuSoap <Kazu.Soap@gmail.com>
2025-01-05 01:03:44 +0900

    add files

---
+++ startup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -e "/etc/init.d/pcscd" ]; then
+  while :; do
+    echo "starting pcscd..."
+    /etc/init.d/pcscd start
+    sleep 1
+    timeout 2 pcsc_scan | grep -A 50 "Using reader plug'n play mechanism"
+    if [ $? = 0 ]; then
+      break;
+    fi
+    echo "failed!"
+  done
+fi
+
+echo "starting b25-server..."
+b25-server &
+
+echo "starting mirakc..."
+/usr/local/bin/mirakc
+
+if [ -e "/etc/init.d/pcscd" ]; then
+  echo "stopping pcscd..."
+  /etc/init.d/pcscd stop
+  sleep 1
+fi
